b23d3ca4aa77ce62b18944840409ba7c4add81da
Miguel Sanchez <msanchez.anaheim@gmail.com>
2018-02-28 19:07:07 -0800

    Added the ability to get and populate list items

--- app.js
+++ app.js
@@ -22,6 +22,9 @@ const ItemCtrl = (function() {
 
     // Public methods
     return {
+        getItems: function() {
+            return data.items;
+        },
         logData: function() {
             return data;
         }
@@ -30,10 +33,26 @@ const ItemCtrl = (function() {
 
 // UI Controller
 const UICtrl = (function() {
+    const UISelectors = {
+        itemList: '#item-list'
+    }
 
     // Public methods
     return {
-        
+        populateItemList: function(items) {
+            let html = '';
+            items.forEach(function(item) {
+                html += `
+                <li class="collection-item" id="item-${item.id}">
+                    <strong>${item.name}: </strong><em>${item.calories}</em>
+                    <a href="#" class="secondary-content"><i class="fa fa-pencil"></i></a>
+                </li>
+                `
+            });
+
+            // Insert list items
+            document.querySelector(UISelectors.itemList).innerHTML = html;
+        }
     }
 
 })();
@@ -44,7 +63,10 @@ const App = (function(ItemCtrl, UICtrl) {
     // Public methods
     return {
         init: function() {
-            console.log('Initializing app');
+            // Fetch items from data structure
+            const items = ItemCtrl.getItems();
+            // Populate list with items
+            UICtrl.populateItemList(items);
         }
     }
 })(ItemCtrl, UICtrl);
